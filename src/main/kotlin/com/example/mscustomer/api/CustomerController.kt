3c3ddacb66667cc0613cf5d7800763cbae1e6ad6
Amir Rojas <amir.rojas@ucb.edu.bo>
2023-03-31 04:35:30 -0400

    primer commit

---
+++ src/main/kotlin/com/example/mscustomer/api/CustomerController.kt
@@ -0,0 +1,28 @@
+package com.example.mscustomer.api
+
+import com.example.mscustomer.service.AccountService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/customer")
+class CustomerController @Autowired constructor(private val accountService: AccountService){
+
+    @Value("7070")
+    lateinit var port: String
+
+    @GetMapping("/test")
+    fun test(): String {
+        val result = accountService.test()
+        return "server port: $port -> $result"
+    }
+
+    @GetMapping("/list")
+    fun getCustomers(): List<String> {
+        return listOf("Customer 1", "Customer 2", "Customer 3") + accountService.list()
+    }
+
+}
---
+++ src/main/kotlin/com/example/mscustomer/service/KeycloakService.kt
@@ -0,0 +1,7 @@
+//package com.example.mscustomer.service
+//
+//import org.springframework.cloud.openfeign.FeignClient
+//import org.springframework.web.bind.annotation.RequestParam
+//
+//@FeignClient(name = "keycloak", url="http://localhost:8080/realms/software"){
+//}
